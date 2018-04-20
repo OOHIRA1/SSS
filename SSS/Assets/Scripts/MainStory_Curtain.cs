c5152559b7d0a8b31592afbbb7d11a1e8e5271f6
oohira <t3e0vbbdwbd0zr6bgftu@docomo.ne.jp>
2018-04-20 19:12:09 +0900

    4/20以前までの最新データインポート
    ・4/20以前までの最新データをインポートしました

---
+++ SSS/Assets/Scripts/MainStory_Curtain.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainStory_Curtain : MonoBehaviour {
+	Animator _animator;
+
+	// Use this for initialization
+	void Start () {
+		_animator = GetComponent<Animator> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.A)) {
+			_animator.SetTrigger ( "OpenFlag" );
+		}
+		if( Input.GetKeyDown (KeyCode.C) ) {
+			_animator.SetTrigger ( "CloseFlag" );
+		}
+
+
+	}
+}
---
+++ SSS/Assets/Scripts/stage_selec_scripts/stage_select_cutain_scripts.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class stage_select_cutain_scripts : MonoBehaviour {
+	Animator _animator;
+
+	// Use this for initialization
+	void Start () {
+		_animator = GetComponent<Animator> ();
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if ( Input.GetKeyDown(KeyCode.A)) {
+        _animator.SetTrigger( "stage_select_T" );
+    }
+			
+
+	}
+}
