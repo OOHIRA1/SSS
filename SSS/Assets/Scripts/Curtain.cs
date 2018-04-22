72744a7ea7f45f665fb01d8c6b2620b32172e92a
oohira <t3e0vbbdwbd0zr6bgftu@docomo.ne.jp>
2018-04-22 15:40:20 +0900

    カーテンのスクリプトの作成
    ・カーテンの操作を管理するスクリプトを作成しました

---
+++ SSS/Assets/Scripts/Curtain.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//==カーテンの機能を管理するクラス
+//
+//使い方：Curtainにアタッチ
+public class Curtain : MonoBehaviour {
+	Animator _animator;
+
+	// Use this for initialization
+	void Start () {
+		_animator = GetComponentInChildren<Animator> ( );
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+	//----------------------------------------------
+	//public関数
+	//----------------------------------------------
+
+	//--カーテンを開ける関数
+	public void Open( ) {
+		_animator.SetTrigger ( "OpenFlag" );
+	}
+
+
+	//--カーテンを閉める関数
+	public void Close( ) {
+		_animator.SetTrigger ( "CloseFlag" );
+	}
+
+	//----------------------------------------------
+	//----------------------------------------------
+
+}
