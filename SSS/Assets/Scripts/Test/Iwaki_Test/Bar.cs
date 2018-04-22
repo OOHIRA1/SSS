4ed21caf936195bb7be39fb534db8577b9600ddb
oohira <t3e0vbbdwbd0zr6bgftu@docomo.ne.jp>
2018-04-22 14:52:30 +0900

    ファイルの整理、シークバーの実験
    ・ファイルの整理をしました
    ・岩城君がシークバーの実験を始めました

---
+++ SSS/Assets/Scripts/Test/Iwaki_Test/Bar.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bar : MonoBehaviour {
+	Transform bar_size;
+	//float x;
+	Vector3 size;
+	// Use this for initialization
+	void Start ( ) {
+		bar_size = GetComponent< Transform > ( );
+		size = new Vector3( 0, 1, 1 );
+		bar_size.localScale = size;
+	}
+
+	// Update is called once per frame
+	void Update ( ) {
+		bar_size.localScale = size;
+		size.x += 0.0001f;
+
+		if (size.x > 1.0f) size.x = 1.0f;
+	}
+
+}
