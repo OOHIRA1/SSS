706e3edd120ffe7289a83ff76ae7de25db2243f9
oohira <t3e0vbbdwbd0zr6bgftu@docomo.ne.jp>
2018-05-02 18:13:47 +0900

    キャラクターのプレハブ化
    ・キャラクターの画像テータを追加しました（３人）
    ・キャラクターのアニメーションを追加しました（３人）
    ・キャラクターのプレハブ化をしました（３人）

---
+++ SSS/Assets/Scripts/Detective_Office_Folder/Detective_Office_Script.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Detective_Office_Script : MonoBehaviour {
+
+	Animator _animator;
+
+	// Use this for initialization
+	void Start () {
+		_animator = GetComponentInChildren<Animator> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		int num_1 = 0;
+
+		//_animator.SetBool( "Bool_01", false );
+
+		if( Input.GetKey ( KeyCode.Q )){
+			num_1 = 1;
+		}
+
+		if( Input.GetKey ( KeyCode.W )){
+			num_1 = 2;
+		}
+
+
+		if( num_1 == 1 ){
+			_animator.SetBool( "boolGameOver", true );
+		} 
+
+		if( num_1 == 2 ){
+			_animator.SetBool( "bool01", true );
+		}
+
+
+		if( Input.GetKey ( KeyCode.P )){
+			_animator.SetBool( "bool01", false );
+			_animator.SetBool( "boolGameOver", false );
+		}
+
+	}
+}
