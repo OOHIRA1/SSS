46127d9e6dab82402d3318049328e3a8132488e0
oohira <t3e0vbbdwbd0zr6bgftu@docomo.ne.jp>
2018-05-13 14:54:10 +0900

    時計UIの編集データのインポート
    ・時計UIの編集データをインポートしました

---
+++ SSS/Assets/Scripts/Test/YuzawaTest/WetherButton.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WetherButton : MonoBehaviour {
+
+	[SerializeField] GameObject[] _clockUi = new GameObject[3];
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	public void ClockUiButton(int ArrayNumber) {
+		bool value = true;
+		for( int i = 0; i <= _clockUi.Length; i++ )
+		{
+			if ( i == ArrayNumber ) {
+				_clockUi[i].SetActive(value);
+				}
+			}
+		}
+	}
