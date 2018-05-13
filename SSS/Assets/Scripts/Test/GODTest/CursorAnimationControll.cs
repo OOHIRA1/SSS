7614855a612505ebcf91e951b33c59170c0cc57a
oohira <t3e0vbbdwbd0zr6bgftu@docomo.ne.jp>
2018-05-13 17:34:26 +0900

    犯人指摘のカーソル追加
    ・犯人指摘用のカーソルを作成しました
    ・アニメーションを実装済み犯人の位置に移動機能はまだ未実装

---
+++ SSS/Assets/Scripts/Test/GODTest/CursorAnimationControll.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//==カーソルのアニメーションを管理するクラス
+//
+//使用方法：カーソルにアタッチ
+public class CursorAnimationControll : MonoBehaviour {
+	Vector3 _firstPosition;					//カーソルの初期位置
+	[SerializeField] float _moveRange = 0;	//アニメーションで動く範囲
+	[SerializeField] float _moveSpeed = 0;	//アニメーションの速さ(unit/second)
+	bool _downFlag;							//アニメーションで下に動いているかのフラグ
+
+	// Use this for initialization
+	void Start () {
+		_firstPosition = transform.position;
+		_downFlag = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float posY = transform.position.y;
+		if (_downFlag) {
+			transform.Translate (0, -_moveSpeed * Time.deltaTime, 0, Space.World);
+			if (posY < _firstPosition.y - _moveRange / 2) {
+				_downFlag = false;
+			}
+		} else {
+			transform.Translate (0, _moveSpeed * Time.deltaTime, 0, Space.World);
+			if (posY > _firstPosition.y + _moveRange / 2) {
+				_downFlag = true;
+			}
+		}
+	}
+
+}
