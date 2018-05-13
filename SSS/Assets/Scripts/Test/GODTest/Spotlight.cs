7909bdb70d52f49120d2cf77f44a28177852241b
oohira <t3e0vbbdwbd0zr6bgftu@docomo.ne.jp>
2018-05-13 23:13:57 +0900

    スポットライトの追加・機能追加、テキストウィンドウの追加＆配置
    ・スポットライト画像を追加しました
    ・スポットライトが犯人指摘カーソルUIの位置に移動するようにしました
    ・テキストウィンドウの画像を追加、配置しました

---
+++ SSS/Assets/Scripts/Test/GODTest/Spotlight.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//==スポットライトを管理するクラス
+//
+//使用方法：スポットライトにアタッチ
+public class Spotlight : MonoBehaviour {
+	[SerializeField] Transform _cursorTransform = null;		//カーソルのTransform
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 pos = transform.position;
+		pos.x = _cursorTransform.position.x;
+		transform.position = pos;
+	}
+}
