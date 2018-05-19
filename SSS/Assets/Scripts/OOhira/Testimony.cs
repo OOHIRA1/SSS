ef5c6f890e7ea0579f0edcc6853d2d57af4f1ea9
oohira <t3e0vbbdwbd0zr6bgftu@docomo.ne.jp>
2018-05-20 03:35:50 +0900

    証言を見る機能追加
    ・探偵がNPCに近づくと証言が表示されるようにしました

---
+++ SSS/Assets/Scripts/OOhira/Testimony.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//==証言をする機能を付けるクラス
+//
+//使用方法：証言をするNPCにアタッチ
+[RequireComponent(typeof(BoxCollider2D))]
+public class Testimony : MonoBehaviour {
+	[SerializeField] GameObject _testimonyBalloon = null;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	//===========================================================
+	//コライダー検出時処理関数
+	void OnTriggerEnter2D( Collider2D col ) {
+		if (col.tag == "Player") {
+			_testimonyBalloon.SetActive (true);
+		}
+	}
+
+	void OnTriggerExit2D( Collider2D col ) {
+		if (col.tag == "Player") {
+			_testimonyBalloon.SetActive (false);
+		}
+	}
+	//============================================================
+	//============================================================
+
+}
---
+++ SSS/Assets/Scripts/OOhira/TextOnSpeechBalloon.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//==吹き出し上で表示されるテキストを管理クラス
+//
+//使用方法：吹き出しのゲームオブジェクトにアタッチ
+public class TextOnSpeechBalloon : MonoBehaviour {
+	Animator _animator;
+	[SerializeField] GameObject[] _text = null;
+
+
+	// Use this for initialization
+	void Start () {
+		_animator = GetComponent<Animator> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		int layer = _animator.GetLayerIndex ("Base Layer");
+		AnimatorStateInfo animatorStateInfo = _animator.GetCurrentAnimatorStateInfo (layer);
+		if (animatorStateInfo.IsName ("SpeechBalloon")) {
+			for (int i = 0; i < _text.Length; i++) {
+				_text[i].SetActive (true);
+			}
+		} else {
+			for (int i = 0; i < _text.Length; i++) {
+				_text[i].SetActive (false);
+			}
+		}
+	}
+}
