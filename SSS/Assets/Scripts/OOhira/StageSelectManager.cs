63627d7530ffcc8474db63c041e27925f5f50c0c
oohira <t3e0vbbdwbd0zr6bgftu@docomo.ne.jp>
2018-05-20 06:41:52 +0900

    ステージ選択シーンの編集
    ・UI配置をしました
    ・白黒アニメのシーンに遷移出来るようにしました

---
+++ SSS/Assets/Scripts/OOhira/StageSelectManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//==StageSelectシーンを管理するクラス
+//
+//使用方法：常にアクティブなゲームオブジェクトにアタッチ
+public class StageSelectManager : MonoBehaviour {
+	[SerializeField] RayShooter _rayShooter = null;
+	[SerializeField] Curtain _curtain = null;
+	[SerializeField] ScenesManager _scenesManager = null;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButtonDown (0)) {
+			RaycastHit2D hit = _rayShooter.Shoot (Input.mousePosition);
+			if (hit) {
+				if (hit.collider.name == "Stage1Button") {
+					_curtain.Close ();
+				}
+			}
+		}
+		if (_curtain.IsStateClose () && _curtain.ResearchStatePlayTime () >= 1f) {
+			_scenesManager.ScenesTransition ("SiteNight_Bedroom");
+		}
+
+		//デバッグ用---------------------------------------
+		if (Input.GetKeyDown (KeyCode.Alpha0)) {
+			_scenesManager.ScenesTransition ("Title");
+		}
+		//------------------------------------------------
+	}
+}
