f603ab4da238588e6b7c333cf2a904b19bb38b90
oohira <t3e0vbbdwbd0zr6bgftu@docomo.ne.jp>
2018-05-27 06:03:46 +0900

    クライマックス問答シーン作成、アニメーション名の変更
    ・クライマックス問答シーンを作成しました
    ・カーテンのアニメーションをスネークケースに変更しました

---
+++ SSS/Assets/Scripts/OOhira/ClimaxBattleManager.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//==クライマックスバトルシーンを管理するクラス
+//
+//使用方法：常にアクティブなゲームオブジェクトにアタッチ
+public class ClimaxBattleManager : MonoBehaviour {
+	enum State {
+		TRUE_OR_FALSE,	//正誤判定
+		INTRODUCTION,	//導入
+		WATCH_MOVIE,	//動画鑑賞
+		CHOOSE_CHOISES,	//選択
+		RESULT			//対決の結果
+	};
+
+	[SerializeField] State _state;
+	GameDataManager _gameDataManager;
+	[SerializeField] Curtain _curtain = null;
+
+	// Use this for initialization
+	void Start () {
+		_state = State.TRUE_OR_FALSE;
+		_gameDataManager = GameObject.FindGameObjectWithTag ("GameDataManager").GetComponent<GameDataManager>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		switch (_state) {
+		case State.TRUE_OR_FALSE:
+			TrueOrFalseAction ();
+			break;
+		case State.INTRODUCTION:
+			IntroductionAction ();
+			break;
+		case State.WATCH_MOVIE:
+			WatchMovieAction ();
+			break;
+		case State.CHOOSE_CHOISES:
+			ChooseChoisesAction ();
+			break;
+		case State.RESULT:
+			ResultAction ();
+			break;
+		default:
+			break;
+		}
+		Debug.Log (_state);
+	}
+
+
+	//--TRUE_OR_FALSEのステートの時の処理をする関数
+	void TrueOrFalseAction(){
+		_gameDataManager.GetCriminal ();
+		_state = State.INTRODUCTION;
+	}
+
+
+	//--INTRODUCTIONのステートの時の処理をする関数
+	void IntroductionAction(){
+		
+	}
+
+
+	//--WATCH_MOVIEのステートの時の処理をする関数
+	void WatchMovieAction(){
+		
+	}
+
+
+	//--CHOOSE_CHOISESのステートの時の処理をする関数
+	void ChooseChoisesAction(){
+		
+	}
+
+
+	//--RESULTのステートの時の処理をする関数
+	void ResultAction(){
+		_curtain.Close ();
+	}
+}
