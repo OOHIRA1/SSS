55192a9c4c67f4a13a0f1834b483286510fccf66
oohira <t3e0vbbdwbd0zr6bgftu@docomo.ne.jp>
2018-05-27 12:44:31 +0900

    富豪の殺人アニメーションの変更（湯澤の作業）
    ・白黒とカラーの画像を切り替えるスクリプトを作成しました

---
+++ SSS/Assets/Scripts/Test/YuzawaTest/MillionareCheck.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MillionareCheck : MonoBehaviour {
+    [SerializeField]GameDataManager _millionareCheck = null;
+    [SerializeField]GameObject _monoDisapper = null;
+    [SerializeField]GameObject _cameraControllManegerDisapper = null;
+
+	// Use this for initialization
+	void Start () {
+        _millionareCheck = GameObject.FindGameObjectWithTag("GameDataManager").GetComponent<GameDataManager>( );
+        if (_millionareCheck.CheckAdvancedDataUntil(GameDataManager.CheckPoint.SHOW_MILLIONARE_MURDER_ANIM)) {
+            _monoDisapper.SetActive(false);
+            _cameraControllManegerDisapper.SetActive(false);
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+    }
+}
