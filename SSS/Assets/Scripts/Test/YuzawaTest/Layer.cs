d112ba6362591550229fc1e46df19834b96e1bf6
oohira <t3e0vbbdwbd0zr6bgftu@docomo.ne.jp>
2018-06-04 02:02:49 +0900

    アップデート

---
+++ SSS/Assets/Scripts/Test/YuzawaTest/Layer.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Layer : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    public void BoxLayer(int num) {
+        var Box = GetComponent<SpriteRenderer>();
+        Box.sortingOrder += num;
+    }
+}
