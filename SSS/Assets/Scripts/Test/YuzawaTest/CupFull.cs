93fb2ff441d5ade6ec4fd4a3b8185cc8cd925e89
oohira <t3e0vbbdwbd0zr6bgftu@docomo.ne.jp>
2018-06-06 20:00:00 +0900

    6/6の最新データのインポート

---
+++ SSS/Assets/Scripts/Test/YuzawaTest/CupFull.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CupFull : MonoBehaviour {
+	[SerializeField]GameObject _cupFull = null;
+	[SerializeField]GameObject _cupEmpty = null;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void Cup(){
+		_cupFull.SetActive (true);
+		_cupEmpty.SetActive (false);
+	}
+}
