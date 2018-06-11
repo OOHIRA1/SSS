bc06089aa2e8ad0d7ae8aeaf5474ad5c8fb8e307
oohira <t3e0vbbdwbd0zr6bgftu@docomo.ne.jp>
2018-06-11 12:20:56 +0900

    クライマックスムービーの研究
    ・ムービー(mp4)を再生・停止・一時停止する方法が分かりました
    ・ムービー(mp4)を特定の位置(frame or time)に移動させる方法が分かりました
    ・ムービー(mp4)を複数個用意し、差し替えても問題なさそうなことが分かりました
    ・今後「特定の位置に移動するシステム」「複数個のムービーを差し替えるシステム」の2パターン用意し対応する予定

---
+++ SSS/Assets/Scripts/OOhira/VideoController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+
+//==Videoを管理するクラス
+//
+//使用方法：VideoPlayerがアタッチされているゲームオブジェクトにアタッチ
+public class VideoController : MonoBehaviour {
+	VideoPlayer _videoPlayer;
+	[SerializeField] long _flame = 0;
+	[SerializeField] double _time = 0;
+	[SerializeField] VideoClip[] _videoClips = null;
+
+	// Use this for initialization
+	void Start () {
+		_videoPlayer = GetComponent<VideoPlayer> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//デバッグ用------------------------------------
+		if (Input.GetKeyDown (KeyCode.A))
+			_videoPlayer.Pause ();
+		if (Input.GetKeyDown (KeyCode.B))
+			_videoPlayer.Play ();
+		if (Input.GetKeyDown (KeyCode.C))
+			_videoPlayer.Stop ();
+		if (Input.GetKeyDown (KeyCode.D))
+			_videoPlayer.frame = _flame;
+		if (Input.GetKeyDown (KeyCode.E))
+			_videoPlayer.time = _time;
+		if (Input.GetKeyDown (KeyCode.Alpha0))
+			_videoPlayer.clip = _videoClips [0];
+		if (Input.GetKeyDown (KeyCode.Alpha1))
+			_videoPlayer.clip = _videoClips [1];
+		Debug.Log (_videoPlayer.frameCount / _videoPlayer.frameRate);
+		Debug.Log (_videoPlayer.time);
+
+		//---------------------------------------------
+	}
+}
