a6d8fc2c291180fc17c6675a4a2a77c1d01dc0e6
oohira <t3e0vbbdwbd0zr6bgftu@docomo.ne.jp>
2018-06-15 11:32:47 +0900

    RayShooter.csの編集
    ・Rayを飛ばさなくする処理を追加しました

--- SSS/Assets/Scripts/OOhira/RayShooter.cs
+++ SSS/Assets/Scripts/OOhira/RayShooter.cs
@@ -7,6 +7,14 @@ using UnityEngine;
 //アタッチ：カメラにアタッチ(カメラから発射するため)
 public class RayShooter : MonoBehaviour {
 	[SerializeField] float _drawRayDistance = 100f;	//描画するRayの長さ(距離)
+	[SerializeField] bool _rayShootable = true;		//Rayを飛ばせるかどうかのフラグ
+
+
+	//============================================================================
+	//セッター
+	public void SetRayShootable( bool x ) { _rayShootable = x; }
+	//============================================================================
+	//============================================================================
 
 	// Use this for initialization
 	void Start () {
@@ -22,6 +30,7 @@ public class RayShooter : MonoBehaviour {
 	//============================================================================
 	//--RayをscreenPointへ飛ばし検出した2Dコライダーの情報(RaycastHit2D)を返す関数
 	public RaycastHit2D Shoot( Vector3 screenPoint ){
+		if (!_rayShootable) return Physics2D.Raycast(Vector2.zero, Vector2.zero);
 		Ray ray = Camera.main.ScreenPointToRay ( screenPoint );
 		RaycastHit2D hit = Physics2D.Raycast (ray.origin, ray.direction);
 		Debug.DrawRay (ray.origin, ray.direction * _drawRayDistance, Color.red);
